5ec372e567ae62146b05075d3f555d746e5d4dea
Apple <apple@Macintosh.local>
2019-07-25 16:45:04 -0400

    new work Emojinal

--- Emojinal/ViewController.swift
+++ Emojinal/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+//    @IBOutlet weak var pageTitle: UILabel!
+//    @IBOutlet weak var pageSubtitle: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+//    @IBAction func shirtTapped(_ sender: UIButton) {
+//    }
+//
+//    @IBAction func faceTapped(_ sender: UIButton) {
+//    }
+    
+    @IBAction func showMessageShirt(sender: UIButton) {
+        let alertShirt = UIAlertController(title:"Treat Yo Self", message: "Go ahead and buy something for yourself!", preferredStyle: UIAlertController.Style.alert)
+        alertShirt.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertShirt, animated: true, completion: nil)
 
-
+        
+//        func showMessage(sender: UIButton) {
+//        let alertMassage = UIAlertController(title:"Treat Yo Self", message: "Get a massage.", preferredStyle: UIAlertController.Style.alert)
+//        alertMassage.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+//
+//        present(alertMassage, animated: true, completion: nil)
+    }
+    @IBAction func showMessageMassage(sender: UIButton) {
+        let alertMassage = UIAlertController(title: "Treat Yo Self", message: "Get a massage.", preferredStyle: UIAlertController.Style.alert)
+      alertMassage.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertMassage, animated: true, completion: nil)
 }
-
+}
